94e9985e6ace74f0856b9172caaa65a242d34b28
Shanarelle <cvl_reid@hotmail.com>
2014-03-27 18:14:35 +0000

    Initial Commit

---
+++ SimpleWeather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by Clare Reid on 27/03/2014.
+//  Copyright (c) 2014 Shanarelle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
